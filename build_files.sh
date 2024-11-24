35625ec50378d648c529a082023e6e68b4c45ed3
jay-only <j79649992@gmail.com>
2024-11-24 16:48:14 +0000

    change python version on vercel build 4

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,7 @@ python3.12 -m pip install -r requirements.txt
 python3.12 -m pip install --upgrade pip
 cd server
 python3.12 -m pip list
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
+
 python3.12 manage.py collectstatic --noinput
