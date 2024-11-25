a5069b23c41c8e8cb078a3ad55cf5f8861060a94
jay-only <j79649992@gmail.com>
2024-11-25 10:26:13 +0000

    update media root to allow for image storage

--- server/server/settings.py
+++ server/server/settings.py
@@ -178,6 +178,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'  # Adjust to your project directory structure
+# MEDIA_ROOT = BASE_DIR / 'media'  # Adjust to your project directory structure
 
-
+MEDIA_ROOT = os.path.join('/tmp', 'media')
